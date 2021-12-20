ae6142941e6c4b98cc69c8d6feea3ecc5fff8f7c
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2021-12-20 08:43:17 -0500

    Questions have been created in Index, generateMarkdown now has a function to generate license badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,116 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please Enter your Projects Title. (Required)",
+      validate: (projectTitle) => {
+        if (projectTitle) {
+          return true;
+        } else {
+          console.log("Cannot leave as a blank value!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username. (Required)",
+      validate: (githubName) => {
+        if (githubName) {
+          return true;
+        } else {
+          console.log("Cannot leave as a blank value!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "githubLink",
+      message: "Enter a link to your GitHub. (Required)",
+      validate: (githubLinkName) => {
+        if (githubLinkName) {
+          return true;
+        } else {
+          console.log("Cannot leave as a blank value!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your Email Address. (Required)",
+      validate: (userEmail) => {
+        if (userEmail) {
+          return true;
+        } else {
+          console.log("Cannot leave as a blank value!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "contact",
+      message: "Please provide details of how to reach you. (Required)",
+      validate: (contactME) => {
+        if (contactME) {
+          return true;
+        } else {
+          console.log("Cannot leave as a blank value!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a detailed description of your project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "Please provide examples and a detailed explanation of your apps usage.",
+    },
+    {
+      type: "list",
+      name: "license",
+      message:
+        "Please select a license (if needed) for your app from the following:",
+      choices: [
+        "The Apache License",
+        "IBM License",
+        "The MIT License",
+        "GNU License",
+        "N/A",
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "Provide details about project contribution. What are some guidelines?.",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Provide some tests for your application and how to run them.",
+    },
+  ]);
+};
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  if (license === "The Apache License") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "IBM License") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+  } else if (license === "The MIT License") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "GNU License") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "N/A") {
+    return "No License Applicable";
+  }
 }
 
 module.exports = generateMarkdown;
